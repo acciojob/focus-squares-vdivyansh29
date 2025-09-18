cc6cc40787d544145300c7ea216275cd26220903
Divyansh Verma <divyanshmotog5g@gmail.com>
2025-09-18 11:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const boxes = document.querySelectorAll(".square")
+
+boxes.forEach(box => {
+        box.addEventListener("mouseover", (e) => bgColor(e));
+        box.addEventListener("mouseout", () => defaultBgColor());
+})
+
+function bgColor(e){
+	const boxesId = e.target
+
+	boxes.forEach(box => {
+		if (box !== boxesId) {
+			box.style.backgroundColor = "#6F4E37";
+		} else {
+			box.style.backgroundColor = "#E6E6FA";
+		}
+	})
+}
+
+function defaultBgColor(){
+	boxes.forEach(box => {
+            box.style.backgroundColor = "#E6E6FA";
+    })
+}
